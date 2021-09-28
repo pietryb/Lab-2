f56f185ad911c314c0dc7e4ed88bf035c1192aa3
pietryb <90348113+pietryb@users.noreply.github.com>
2021-09-28 09:22:51 -0400

    Add files via upload

---
+++ LAB 2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the room calculator");
+
+            Console.WriteLine("What is the length of the room?");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the length of the room?");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = width * length;
+            double perimeter = (width + length);
+            double paint = perimeter / 5;
+            double carpet = area / 5;
+
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine($"Perimeter: {perimeter}");
+            Console.WriteLine($"Paint: {paint}");
+            Console.WriteLine($"Carpet: {carpet}");
+
+
+        }
+    }
+}
